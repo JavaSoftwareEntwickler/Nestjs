e73ed24d7031adffd989d534e7479e219c15faa5
JavaSoftwareEntwickler <mm.java.software@gmail.com>
2023-09-17 18:44:58 +0200

    Aggiunto decorator custom e Guard
    decorator per usarlo nel controller e restituire info sullo user loggato
    invece che utilizzare sul controller la Express Request e
     prendere le informazioni da li
    e Guard per non usare nel controller la classe AuthGuard
    alla quale si possono passare parametri diversi quindi
    implementabile con una classe che la estende

---
+++ src/auth/decorator/get-user.decorator.ts
@@ -0,0 +1,13 @@
+import { ExecutionContext, createParamDecorator } from "@nestjs/common";
+
+export const GetUser = createParamDecorator(
+    (data: string | undefined, ctx: ExecutionContext) => {
+        const request: Express.Request = ctx
+        .switchToHttp()
+        .getRequest();
+        if(data){
+            return {id: request.user[data]}
+        }
+        return request.user;
+    },
+);
